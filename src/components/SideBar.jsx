65ee5ca5bdd1956c39314375917e3f95f4ad3e77
Joao <joca2016gato@gmail.com>
2023-11-03 17:37:34 +0200

    updated 2

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -16,7 +16,7 @@ const SideBar = ({selectedCategory, setSelectedCategory}) => (
         selectedCategory && '#FC1503', color: 'white' }}
         key={category.name}
         >
-          <span style= {{ color: category.name === selectedCategory ? 'white' : 'red', MarginRight:'15px'}}>{category.icon}</span>
+          <span style= {{ color: category.name === selectedCategory ? 'white' : 'red', marginRight:'15px'}}>{category.icon}</span>
           <span style= {{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
 
         </button>
--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,6 @@ export {default as SearchFeed} from './SearchFeed';
 export {default as SearchBar} from './SearchBar';
 export {default as SideBar} from './SideBar';
 export {default as  Videos} from './Videos';
+export {default as  VideosCard} from './VideosCard';
+export {default as  ChannelCard} from './ChannelCard';
+export { default as Loader } from './Loader';
